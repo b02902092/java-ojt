9d00739638ebe45581a4e327e9fe2724b230c477
b02902092 <b02902092@ntu.edu.tw>
2025-06-13 13:11:29 +0900

    Handle type mismatch

--- src/main/java/com/example/javaOjt/exceptions/handlers/OjtExceptionHandler.java
+++ src/main/java/com/example/javaOjt/exceptions/handlers/OjtExceptionHandler.java
@@ -1,17 +1,36 @@
 package com.example.javaOjt.exceptions.handlers;
 
 import com.example.javaOjt.beans.responses.common.OjtExceptionResponse;
+import com.example.javaOjt.exceptions.OjtBadRequestException;
 import com.example.javaOjt.exceptions.OjtExceptionBase;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 @Slf4j
 public class OjtExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @Override
+  protected ResponseEntity<Object> handleTypeMismatch(
+    @NonNull TypeMismatchException ex,
+    @NonNull HttpHeaders headers,
+    @NonNull HttpStatusCode status,
+    @NonNull WebRequest request) {
+
+    Object[] args = {ex.getPropertyName(), ex.getValue()};
+    String defaultDetail = "Failed to convert '" + args[0] + "' with value: '" + args[1] + "'";
+
+    return handleOjtException(new OjtBadRequestException(defaultDetail));
+  }
 
   /**
    * OJT Exception Handler
