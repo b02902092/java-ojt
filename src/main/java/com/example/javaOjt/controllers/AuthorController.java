c6bb09882fcc39d87433a4fabc0ad2408bbf366e
b02902092 <b02902092@ntu.edu.tw>
2025-06-25 20:13:53 +0900

    Remove @Validated, see https://tech.excite.co.jp/entry/2021/04/30/073136

--- src/main/java/com/example/javaOjt/controllers/AuthorController.java
+++ src/main/java/com/example/javaOjt/controllers/AuthorController.java
@@ -5,7 +5,6 @@ import com.example.javaOjt.exceptions.OjtNotFoundException;
 import com.example.javaOjt.services.AuthorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/authors")
 @RequiredArgsConstructor
-@Validated
 public class AuthorController {
 
   private final AuthorService authorService;
--- src/main/java/com/example/javaOjt/controllers/HealthCheckController.java
+++ src/main/java/com/example/javaOjt/controllers/HealthCheckController.java
@@ -1,18 +1,19 @@
 package com.example.javaOjt.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health-check/")
+@RequestMapping("/health-check")
 @RequiredArgsConstructor
 @Validated
 public class HealthCheckController {
 
-  @RequestMapping("/")
-  public String healthCheck() {
-    return "OK";
+  @RequestMapping("")
+  public ResponseEntity<String> healthCheck() {
+    return ResponseEntity.ok("OK");
   }
 }
